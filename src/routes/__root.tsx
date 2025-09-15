ad2ef94a326502e10ecca395a6e730bad2dcf47c
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-09-15 18:05:09 -0300

    moved meta to root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,21 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanstackDevtools } from '@tanstack/react-devtools'
 
 export const Route = createRootRoute({
+
+  head: () => ({
+    meta: [
+      {
+      name: 'description',
+      content: 'Writer notes for my writting process - @ helio sales',
+      }
+    ]
+  }),
+
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanstackDevtools
         config={{
